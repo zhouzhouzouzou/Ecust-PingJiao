366f223ea2e87b79d586a1dbc47646d870bf5b6f
zhouzhouzouzou <734487605@qq.com>
2016-12-11 14:21:49 +0800

    准备开始写提交表单
    正在考虑正则表达式获取内容的问题

--- 20161211_Ecust_PingJiao.py
+++ 20161211_Ecust_PingJiao.py
@@ -22,17 +22,27 @@ def main(sno=None,password=None):
         spider = re.findall('href="pg\.php+(.*?)"',login.text,re.S)
         length = len(spider)
         spiderList = []
-        for i in range(0, length - 1):
-            spiderList.append('http://pjb.ecust.edu.cn/pingce/pg.php' + spider[i])
         pingCe = []
         for i in range(0, length - 1):
+            spiderList.append('http://pjb.ecust.edu.cn/pingce/pg.php' + spider[i])
             pingCe.append(r.get(url=spiderList[i]).text)
-        print pingCe
-
         cookies = r.cookies
         br.set_cookiejar(cookies)
+        pj_normal(spiderList[0])
+
 
     except Exception , e:
         print Exception,':',e
+        return 'Found a bug. Please contact the author.'
+
+def pj_normal(url):
+    r = br.open(url)
+    br.select_form(name='Form1')
+    html = r.read()
+    for i in range(1,20):
+        pattern = re.compile('name="yq+str(i)"')
+        match = pattern.match(html)
+        for value in values:
+            br.form['yq'+str(i)] = [match.group()]
 
 main('10150982','125018')
