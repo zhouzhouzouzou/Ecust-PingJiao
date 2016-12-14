65369ee8f29ddef492f503a45083588e77196620
zf Gong <i@coxxs.com>
2016-12-14 21:51:12 +0800

    Update run.php

--- index/run.php
+++ index/run.php
@@ -1,5 +1,8 @@
 <?php 
-$pw = $_GET['pw'];
-$id = $_GET['id'];
+$pw = (string)($_GET['pw'] ?? '');
+$id = (string)($_GET['id'] ?? '');
+if (!ctype_digit($id) || strlen($id) !== 8 || !ctype_digit($pw) || strlen($pw) !== 6) {
+ exit;
+}
 print passthru('python Ecust_PingJiao.py '.$id.'x'.$pw);
 ?>
